d62d4aaa24e11b757c0077255ea14683267b59b3
张杰 <Jason_Msbaby@163.com>
2016-04-12 14:06:13 +0800

    123

--- E-flyer/Scence/Home/Controller/HomeAddInfoController.m
+++ E-flyer/Scence/Home/Controller/HomeAddInfoController.m
@@ -5,11 +5,22 @@
 //  Created by Jason_Msbaby on 16/3/8.
 //  Copyright © 2016年 Jason_Msbaby. All rights reserved.
 //
-
+#import "LrdAlertTableView.h"
 #import "HomeAddInfoController.h"
 #import "SVProgressHUD.h"
 
 @interface HomeAddInfoController ()
+@property (weak, nonatomic) IBOutlet UILabel *subMoney;//总计金额
+@property (weak, nonatomic) IBOutlet UIButton *btn_pay;//支付按钮
+@property (weak, nonatomic) IBOutlet UITextField *txt_title;//标题
+@property (weak, nonatomic) IBOutlet UITextView *txt_content;//内容
+@property (weak, nonatomic) IBOutlet UIButton *btn_addFile;//添加图片或者视频
+@property (weak, nonatomic) IBOutlet UITextField *txt_question;//问题
+@property (weak, nonatomic) IBOutlet UITextField *txt_answer;//答案
+@property (weak, nonatomic) IBOutlet UITextField *txt_price;//单价
+@property (weak, nonatomic) IBOutlet UITextField *txt_number;//数量
+@property (weak, nonatomic) IBOutlet UISwitch *switch_isUp;//是否推荐
+@property (weak, nonatomic) IBOutlet UIView *dropBaseView;//下拉菜单的占位view
 
 
 @end
@@ -22,6 +33,13 @@
     self.view.backgroundColor = kRandomColor;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"down_close"] style:(UIBarButtonItemStyleDone) target:self action:@selector(leftBarButtonItemClick:)];
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"share"] style:(UIBarButtonItemStyleDone) target:self action:@selector(rightBarButtonItemClick:)];
+    [self layoutDropDownView];
+}
+/*!
+ *  添加 针对人群 针对城市 所属类别进行添加下拉菜单
+ */
+- (void)layoutDropDownView{
+    
 }
 
 
@@ -33,6 +51,10 @@
 - (void)rightBarButtonItemClick:(UIBarButtonItem *)sender{
     [SVProgressHUD showInfoWithStatus:@"分享"];
 }
+
+
+
+#pragma mark - 其他控件事件处理
 
 
 @end
