9e654f5670af8f586bcde0e5859e8be109906d89
Âº†Êù∞ <Jason_Msbaby@163.com>
2016-04-17 12:42:48 +0800

    创商品管理控制器

---
+++ E-flyer/Scence/Me/Controller/SecondVC/MeInfoController.h
@@ -0,0 +1,13 @@
+//
+//  MeInfoController.h
+//  E-flyer
+//  个人中心信心展示通用controller
+//  Created by Jason_Msbaby on 16/4/17.
+//  Copyright © 2016年 Jason_Msbaby. All rights reserved.
+//
+#import "MeMenu.h"
+#import "BasicController.h"
+
+@interface MeInfoController : BasicController
+@property(strong,nonatomic) MeMenu *menu;
+@end
